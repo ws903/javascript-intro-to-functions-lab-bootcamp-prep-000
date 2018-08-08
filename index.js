e1efaa312496752811dbedf89712ee0e3df4344b
David Sim <wsim903@gmail.com>
2018-08-08 01:29:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,11 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if string.
+  if string.toLowerCase() === string{
+    return "I can't hear you!"
+  else if string.toUpperCase() === string{
+    return "YES INDEED!"
+  
+  }
+  }
 }
